39926dfb7e22bc8256fb29503364bcb6b162cedc
Brad Warren <brad.warren@aa.com>
2025-09-22 21:18:18 -0700

    flight fields working

--- FlightService/FlightService/Services/FlightService.cs
+++ FlightService/FlightService/Services/FlightService.cs
@@ -7,7 +7,7 @@ namespace FlightService.Services
         Flight IFlightService.getFlight(int id)
         {
             logger.LogInformation("Creating flight with id " + id);  //TODO make this structured (json)
-            return new Flight(id);
+            return new Flight(id, new DateOnly(2025, 9, 11), 1234, "ABC", "DEF", 'S', new DateTimeOffset(2025, 9, 11, 10, 5, 0, new TimeSpan(7, 0, 0)), new DateTimeOffset(2025, 9, 11, 12, 15, 0, new TimeSpan(5, 0, 0)));
         }
     }
 }
